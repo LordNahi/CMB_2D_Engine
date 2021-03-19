27ebc76d9608fd94b66d4447b9de1d612c5843ec
Nathan Nahi <nathannahisteam@gmail.com>
2021-03-18 19:05:41 -0700

    Initial Commit

---
+++ build/CMakeFiles/SFML_template.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nathan/Documents/projects/game_engine/src/main.cpp" "/home/nathan/Documents/projects/game_engine/build/CMakeFiles/SFML_template.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "AL_LIBTYPE_STATIC"
+  "FLAC__NO_DLL"
+  "HAS_FCHOWN"
+  "HAS_STICKY_DIR_BIT"
+  "SFML_STATIC"
+  "STB_TEXTEDIT_KEYTYPE=unsigned"
+  "_BSD_SOURCE"
+  "_DEFAULT_SOURCE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/nathan/.conan/data/sfml/2.5.1/bincrafters/stable/package/1d45e316e71aa76e122414be8f4c0d6688deabd4/include"
+  "/home/nathan/.conan/data/freetype/2.10.1/_/_/package/1381eb533150a52ee50820711faa223d436deba1/include"
+  "/home/nathan/.conan/data/freetype/2.10.1/_/_/package/1381eb533150a52ee50820711faa223d436deba1/include/freetype2"
+  "/home/nathan/.conan/data/libpng/1.6.37/_/_/package/1e8b7ff23bd5e7932ba7e8874349125fdf8e91ec/include"
+  "/home/nathan/.conan/data/zlib/1.2.11/_/_/package/6b7ff26bfd4c2cf2ccba522bfba2d2e7820e40da/include"
+  "/home/nathan/.conan/data/bzip2/1.0.8/_/_/package/ff2ff6bb41ed84283a6634bada252c37f13eb93c/include"
+  "/home/nathan/.conan/data/stb/20200203/_/_/package/5ab84d6acfe1f23c4fae0ab88f26e3a396351ac9/include"
+  "/home/nathan/.conan/data/openal/1.19.1/_/_/package/395a697543c6d91c9cea7263a9a7a1fd3e6ce3fd/include"
+  "/home/nathan/.conan/data/openal/1.19.1/_/_/package/395a697543c6d91c9cea7263a9a7a1fd3e6ce3fd/include/AL"
+  "/home/nathan/.conan/data/libalsa/1.2.4/_/_/package/847849d4cc4b6352c8023780ae6930933f613ad9/include"
+  "/home/nathan/.conan/data/flac/1.3.3/_/_/package/18e127a3cdb0f6639b6def6067ba912bdf1053a3/include"
+  "/home/nathan/.conan/data/ogg/1.3.4/_/_/package/6b7ff26bfd4c2cf2ccba522bfba2d2e7820e40da/include"
+  "/home/nathan/.conan/data/vorbis/1.3.6/_/_/package/b64f670df847327ead52ac5bfa3722faedc2edc3/include"
+  "/usr/include/uuid"
+  "/usr/include/freetype2"
+  "/usr/include/libpng16"
+  "/home/nathan/.conan/data/fmt/7.1.3/_/_/package/f54287fc3dbd09590f968b580bebb565ea4a4b17/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ readme.md
@@ -0,0 +1,22 @@
+
+# Building with CMake (Linux)
+
+## Requirements
+* conan
+* cmake
+
+## Build
+
+Create build directory and switch to it
+```
+mkdir build && cd build
+````
+
+Run cmake
+```
+cmake ..
+```
+
+```
+cmake --build .
+```
