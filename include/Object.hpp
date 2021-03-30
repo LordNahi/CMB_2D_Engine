06f8e7114eca695378bbba7790cf78b70e6a1b85
Nathan Nahi <nathannahisteam@gmail.com>
2021-03-29 22:20:02 -0700

    Implement basic ECS code

---
+++ include/Object.hpp
@@ -0,0 +1,68 @@
+#ifndef Object_hpp
+#define Object_hpp
+
+#include <memory>
+#include <vector>
+
+#include "Component.hpp"
+#include "Window.hpp"
+
+class Object
+{
+    public:
+        // Awake is called when object is created, used
+        // to ensure required components are present ...
+        void Awake();
+
+        // Called after Awake() ...
+        void Start();
+
+        void Update(float deltaTime);
+        void LateUpdate(float deltaTime);
+        void Draw (Window& window);
+
+        template <typename T> std::shared_ptr<T> AddComponent()
+        {
+            // This ensures that we only try to add a class that derives
+            // from Component. This is tested at compile time.
+            static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
+
+            // Check we don't already have this component ...
+            for (auto& component : components)
+            {
+                // Currently prevent the same component being added twice ...
+                if (std::dynamic_pointer_cast<T>(component))
+                {
+                    return std::dynamic_pointer_cast<T>(component);
+                }
+            }
+
+            // The object does nothave this component so we create it and
+            // add it to out components list ...
+            std::shared_ptr<T> newComponent = std::make_shared<T>(this);
+            components.push_back(newComponent);
+
+            return newComponent;
+        }
+
+        template <typename T> std::shared_ptr<T> GetComponent()
+        {
+            static_assert(std::is_base_of<Component, T>::value, "T must derive from Component");
+
+            // Check if component exists in out 
+            for (auto& component : components)
+            {
+                if (std::dynamic_pointer_cast<T>(component))
+                {
+                    return std::dynamic_pointer_cast<T>(component);
+                }
+            }
+
+            return nullptr;
+        }
+
+    private:
+        std::vector<std::shared_ptr<Component>> components;
+};
+
+#endif /* Object_hpp */
---
+++ src/Object.cpp
@@ -0,0 +1,41 @@
+#include "Object.hpp"
+
+void Object::Awake()
+{
+    for (int i = components.size() - 1; i >= 0; i --)
+    {
+        components[i]->Awake();
+    }
+}
+
+void Object::Start()
+{
+    for (int i = components.size() - 1; i >= 0; i --)
+    {
+        components[i]->Start();
+    }
+}
+
+void Object::Update(float deltaTime)
+{
+    for (int i = components.size() - 1; i >= 0; i --)
+    {
+        components[i]->Update(deltaTime);
+    }
+}
+
+void Object::LateUpdate(float deltaTime)
+{
+    for (int i = components.size() - 1; i >= 0; i --)
+    {
+        components[i]->LateUpdate(deltaTime);
+    }
+}
+
+void Object::Draw(Window& window)
+{
+    for (int i = components.size() - 1; i >= 0; i --)
+    {
+        components[i]->Draw(window);
+    }
+}
