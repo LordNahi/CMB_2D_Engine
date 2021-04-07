b43857615ba1b799a7300385194ac62ce7b000b4
Nathan Nahi <nathannahisteam@gmail.com>
2021-04-06 21:25:30 -0700

    Change default window dimensions

--- src/Engine/Window.cpp
+++ src/Engine/Window.cpp
@@ -1,7 +1,7 @@
 #include "Engine/Window.hpp"
 
 Window::Window(const std::string& windowName)
-    : window(sf::VideoMode(800, 600), windowName, sf::Style::Close)
+    : window(sf::VideoMode(640, 480), windowName, sf::Style::Close)
 {
     window.setVerticalSyncEnabled(true);
 }
