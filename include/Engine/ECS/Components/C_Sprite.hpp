146c053aabe68ebe5dab0360bef94dbd767f8596
Nathan Nahi <nathannahisteam@gmail.com>
2021-04-28 16:43:57 -0700

    Expose new Sprite functionality

--- include/Engine/ECS/Components/C_Sprite.hpp
+++ include/Engine/ECS/Components/C_Sprite.hpp
@@ -12,12 +12,17 @@ class C_Sprite : public Component
         // Loads a sprite from file ...
         void Load(const std::string& filePath);
         void Load(int id);
-
+        void FlipX();
+        void FlipY();
         void SetTextureAllocator(ResourceAllocator<sf::Texture>* allocator);
         void SetTextureRect(int x, int y, int width, int height);
         void SetTextureRect(const sf::IntRect& rect);
         void SetScale(float scale);
         void SetScale(float x, float y);
+        void SetOrigin(float origin);
+        void SetOrigin(float x, float y);
+        sf::Vector2f GetScale();
+        sf::Vector2f GetOrigin();
 
         // Override Component's Draw method ...
         void Draw(Window& window) override;
@@ -26,9 +31,12 @@ class C_Sprite : public Component
         void LateUpdate(float deltaTime) override;
 
     private:
-        int currentTextureID = -1;
         ResourceAllocator<sf::Texture>* allocator;
         sf::Sprite sprite;
+
+        int currentTextureID = -1;
+        sf::Vector2f originNorm = {0.f, 0.f};
+
 };
 
 #endif /* C_Sprite */
--- src/Engine/ECS/Components/C_Sprite.cpp
+++ src/Engine/ECS/Components/C_Sprite.cpp
@@ -45,7 +45,7 @@ void C_Sprite::LateUpdate(float deltaTime)
 
 void C_Sprite::SetScale(float scale)
 {
-    sprite.setScale(scale, scale);
+    SetScale(scale, scale);
 }
 
 void C_Sprite::SetScale(float x, float y)
@@ -53,9 +53,56 @@ void C_Sprite::SetScale(float x, float y)
     sprite.setScale(x, y);
 }
 
+void C_Sprite::SetOrigin(float origin)
+{
+    SetOrigin(origin, origin);
+}
+
+void C_Sprite::SetOrigin(float x, float y)
+{
+    /** Storing the originNorm so when using `SetTextureRect()`
+     * it can apply the origin to new texture rect ...
+     */
+
+    originNorm.x = x;
+    originNorm.y = y;
+
+    auto bounds = sprite.getLocalBounds();
+
+    sprite.setOrigin({
+        bounds.width * x,
+        bounds.height * y
+    });
+}
+
+sf::Vector2f C_Sprite::GetScale()
+{
+    return sprite.getScale();
+}
+
+sf::Vector2f C_Sprite::GetOrigin()
+{
+    return sprite.getOrigin();
+}
+
+void C_Sprite::FlipX()
+{
+    auto scale = GetScale();
+
+    SetScale(-scale.x, scale.y);
+}
+
+void C_Sprite::FlipY()
+{
+    auto scale = GetScale();
+
+    SetScale(scale.x, -scale.y);
+}
+
 void C_Sprite::SetTextureRect(int x, int y, int width, int height)
 {
     sprite.setTextureRect(sf::IntRect(x, y, width, height));
+    sprite.setOrigin(width * originNorm.x, height * originNorm.y);
 }
 
 void C_Sprite::SetTextureRect(const sf::IntRect& rect)
